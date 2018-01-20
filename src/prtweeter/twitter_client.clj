b8a380c0aa06ba64ab2f5b19ce0610cf1503541f
David Ongaro <david.ongaro@hamburg.de>
2018-01-20 21:02:45 +0800

    Just skip duplicate status updates
    These might happen in case of a race condition or if the user set back
    the earliest-pr date

--- src/prtweeter/twitter_client.clj
+++ src/prtweeter/twitter_client.clj
@@ -1,7 +1,10 @@
 (ns prtweeter.twitter-client
-  (:require [prtweeter.helper :refer [abort]]
-            [twitter.api.restful :as twitter]
-            [twitter.oauth :as twitter-oauth]))
+  (:require [twitter.api.restful :as twitter]
+            [twitter.callbacks.handlers
+             :refer
+             [exception-rethrow response-return-body response-throw-error]]
+            [twitter.oauth :as twitter-oauth])
+  (:import twitter.callbacks.protocols.SyncSingleCallback))
 
 (defn default-error [e]
   (str "\nEncountered a problem when contacting twitter:\n\n" e
@@ -21,5 +24,15 @@
 (defn status-update [credentials text]
   (twitter/statuses-update
    :oauth-creds (credentials->oauth credentials)
-   :params {:status text})
-  )
+   :params {:status text}
+   :on-exception (fn [response] (print "GOT" response))
+   :callbacks (SyncSingleCallback.
+               response-return-body
+               (fn [response]
+                 (if-let [duplicate-error
+                          (some (fn [error] (and (= (error "code") 187) error))
+                                ((clojure.data.json/read-str (str @(:body response))) "errors"))]
+                   (printf "Got '%s' Skipping\n" (duplicate-error "message"))
+                   (response-throw-error response)))
+               exception-rethrow)
+  ))
